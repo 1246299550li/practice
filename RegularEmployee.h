235e9a2bac1eb0bdaeb315a119318fdf9fb6be3b
1246299550li <1246299550@qq.com>
2019-01-12 15:36:21 +0800

    base info

--- RegularEmployee.h
+++ RegularEmployee.h
@@ -15,13 +15,107 @@ private:
     double providentFund; //住房公积金
     double pension; //养老金
     double tax; //所得税+
-    double incsurance; //医疗保险
+    double insurance; //医疗保险
     double realWage; //实发工资
     // 计算实发工资=基本职务工资+岗位津贴-养老金-住房公积金-所得税-医疗保险
     void calculateRealWage() override {
-        realWage = baseWage + allowance - pension - providentFund - tax - incsurance;
+        realWage = baseWage + allowance - pension - providentFund - tax - insurance;
     };
-    bool readInfo(int id, string name, bool sex, int age, string address, double baseWage, double allowance, double providentFund, double pension, double tax, double incsurance, double realWage)
+public:
+    RegularEmployee(double allowance, double providentFund, double pension, double tax, double incsurance,
+                    double realWage) : allowance(allowance), providentFund(providentFund), pension(pension), tax(tax),
+                                       insurance(insurance), realWage(realWage) {
+        calculateRealWage();
+    }
+
+    void setAllowance(double allowance) {
+        RegularEmployee::allowance = allowance;
+    }
+
+    void setProvidentFund(double providentFund) {
+        RegularEmployee::providentFund = providentFund;
+    }
+
+    void setPension(double pension) {
+        RegularEmployee::pension = pension;
+    }
+
+    void setTax(double tax) {
+        RegularEmployee::tax = tax;
+    }
+
+    void setIncsurance(double insurance) {
+        RegularEmployee::insurance = insurance;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void setRealWage(double realWage) {
+        RegularEmployee::realWage = realWage;
+    }
 };
 
 
