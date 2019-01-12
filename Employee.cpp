5d348725b5e2e917aff06f51585d0102cad9a3ba
1246299550li <1246299550@qq.com>
2019-01-12 11:11:51 +0800

    new

---
+++ Employee.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xunmi on 2019/1/12.
+//
+
+#include "Employee.h"
---
+++ Employee.h
@@ -0,0 +1,14 @@
+//
+// Created by xunmi on 2019/1/12.
+//
+
+#ifndef NEW_EMPLOYEE_H
+#define NEW_EMPLOYEE_H
+
+
+class Employee {
+
+};
+
+
+#endif //NEW_EMPLOYEE_H
