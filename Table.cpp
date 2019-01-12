873cf699129e3e80959082bbbbfd6859ccd8f188
Acce1X <605000993@qq.com>
2019-01-12 15:59:19 +0800

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -5,13 +5,19 @@
 #include "Table.h"
 #include<fstream>
 Table::Table() {
-    tableArr = new Employee *[ARR_SIZE];
-    ifstream infile1("RegStaffInf.txt");
-    RegularEmployee *p;
+    tableArr = new Employee *[ARR_SIZE];    //总表
+    
+    ifstream infile1("RegStaffInf.txt");    //读取 
+    RegularEmployee *p;                     //缓存对象
     while(!infile1.eof()){
         p=new RegularEmployee;
-
+        double tmpAllowance,tmpProvidentFund,tmpPension,tmpTax,tmpInsurance;
+        infile1>>tmpAllowance;
+        p->setAllowance(tmpAllowance);
+        infile1>>tmpProvidentFund;
     }
+    
+    
 
 
 }
